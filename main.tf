4f9705020e2adfbc018f661f57e441e904be91fd
jessica <jessica@U-3EBBPKC0407LY.eu-central.gg4l.directory>
2025-07-16 22:16:24 +0300

    EKS Terraform project

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.region}a", "${var.region}b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+  default = {
+    desired_size   = 1
+    max_size       = 1
+    min_size       = 1
+
+    instance_types = ["t2.micro"]
+    capacity_type  = "ON_DEMAND"
+  }
+}
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
